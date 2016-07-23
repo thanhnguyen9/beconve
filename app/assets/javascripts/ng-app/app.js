2a66143e048d5bbecf716250233bb86a27e18155
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-07-22 22:31:24 -0500

    Integrate Angular and Rails

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,11 @@
+angular
+    .module('AngularRails', [
+        'ngRoute',
+        'templates'
+    ]).config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        });
+});
