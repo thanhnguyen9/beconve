0330c8fd0f1b048f8157733050ee27292c37d3ca
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-07-28 22:03:46 -0500

    Heroku test

--- app/assets/javascripts/ng-app/services/workflow/workflow.js
+++ app/assets/javascripts/ng-app/services/workflow/workflow.js
@@ -1,13 +1,13 @@
-angular
-    .module('BeConve')
-
-    .service('Workflow', function($sessionStorage){
-
-        this.get = function(prop){
-            $sessionStorage[prop]
-        };
-
-        this.set = function(prop, value){
-            $sessionStorage[prop] = value;
-        }
-});
+//angular
+//    .module('BeConve')
+//
+//    .service('Workflow', function($sessionStorage){
+//
+//        this.get = function(prop){
+//            $sessionStorage[prop]
+//        };
+//
+//        this.set = function(prop, value){
+//            $sessionStorage[prop] = value;
+//        }
+//});
