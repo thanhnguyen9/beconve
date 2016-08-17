cedf8cb44eba0f859dd1dde5238024de6f8f69c8
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-08-16 22:30:14 -0500

    Adding STRIPE key to .env file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,8 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :stripe_connect,
-                  'ca_91LzmXJ3fnDopWxgUXAWoQpzyFQIWcQZ',
-                  'sk_test_A2pDWi0GI0dTVkvEzkfLuFdG',
+                  ENV['STRIPE_CLIENT_ID'],
+                  ENV["STRIPE_SECRET_KEY"],
                   :scope => 'read_write',
                   :stripe_landing => 'register'
 end
