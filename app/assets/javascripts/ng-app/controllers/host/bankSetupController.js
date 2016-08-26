17b6499d4b16a7e116bc7ab27c90f1c4794c50ff
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-08-25 20:56:48 -0500

    Check if user have already set up STRIPE or not

--- app/assets/javascripts/ng-app/controllers/host/bankSetupController.js
+++ app/assets/javascripts/ng-app/controllers/host/bankSetupController.js
@@ -1,8 +1,20 @@
 angular.module('BeConve')
-    .controller('bankSetupController', ['$scope', '$location', '$window', 'Auth', function($scope, $location, $window, Auth) {
+    .controller('bankSetupController', ['$scope', '$location', '$window', 'Auth', 'Technician',
+        function($scope, $location, $window, Auth, Technician) {
 
         Auth.currentUser().then(function(user) {
-            console.log(Auth._currentUser);
+            var tech = Technician.get({ id: user.id }, function() {
+                $scope.stripe_uid = tech.response.stripe_uid;
+                $scope.stripe_publishable_key = tech.response.stripe_publishable_key;
+                $scope.stripe_publishable_key = tech.response.stripe_publishable_key;
+                $scope.stripe_access_code = tech.response.stripe_access_code;
+
+                if($scope.stripe_uid && $scope.stripe_publishable_key && $scope.stripe_publishable_key && $scope.stripe_access_code){
+                    $scope.stripe_setup = true
+                }else{
+                    $scope.stripe_setup = false
+                }
+            })
         }, function(error) {
             $location.path('/')
         });
