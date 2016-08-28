7c6416caca1bcdc4f91815e7767df8e724709ed5
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-08-28 12:31:26 -0500

    Redesign samsung price setup

--- app/assets/javascripts/ng-app/controllers/host/samsungPriceSetupController.js
+++ app/assets/javascripts/ng-app/controllers/host/samsungPriceSetupController.js
@@ -7,88 +7,7 @@ angular.module('BeConve')
             $location.path('/')
         });
 
-        $scope.samsungScreens = [
-            {
-                name: 'note',
-                price: 0
-            },
-            {
-                name: 'note 2',
-                price: 0
-            },
-            {
-                name: 'note 3',
-                price: 0
-            },
-            {
-                name: 'S3',
-                price: 0
-            },
-            {
-                name: 'S4',
-                price: 0
-            },
-            {
-                name: 'S5',
-                price: 0
-            }
-        ];
-
-        $scope.samsungBatteries = [
-            {
-                name: 'note',
-                price: 0
-            },
-            {
-                name: 'note 2',
-                price: 0
-            },
-            {
-                name: 'note 3',
-                price: 0
-            },
-            {
-                name: 'S3',
-                price: 0
-            },
-            {
-                name: 'S4',
-                price: 0
-            },
-            {
-                name: 'S5',
-                price: 0
-            }
-        ];
-
-        $scope.samsungNotOns = [
-            {
-                name: 'note',
-                price: 0
-            },
-            {
-                name: 'note 2',
-                price: 0
-            },
-            {
-                name: 'note 3',
-                price: 0
-            },
-            {
-                name: 'S3',
-                price: 0
-            },
-            {
-                name: 'S4',
-                price: 0
-            },
-            {
-                name: 'S5',
-                price: 0
-            }
-        ];
-
-        $scope.samsungLiquids = [
+        $scope.samsungModels = [
             {
                 name: 'note',
                 price: 0
@@ -116,22 +35,10 @@ angular.module('BeConve')
         ];
 
         //SAMSUNG
-        $scope.samsungBrokenScreenSubmit = function(){
-            console.log('inside samsung broken screen submit');
-            console.log($scope.samsungScreens);
-        };
-
-        $scope.samsungFaultyBatterySubmit = function(){
-            console.log('inside samsung faulty battery submit');
-            console.log($scope.samsungBatteries);
-        };
-        $scope.samsungNotTurnOnSubmit = function(){
-            console.log('inside samsung not on submit');
-            console.log($scope.samsungNotOns);
-        };
-        $scope.samsungLiquidDamageSubmit = function(){
-            console.log('inside samsung liquid damage submit');
-            console.log($scope.samsungLiquids);
+        $scope.samsungPriceSubmit = function(model, type){
+            console.log('inside samsung' + type + 'submit');
+            console.log(model.name);
+            console.log(model.price);
         };
 
     }]);
