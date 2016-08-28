8392b8d6cecb4a2472ae32614946139421d1443b
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-08-28 12:17:55 -0500

    Redesign Ipad price setup

--- app/assets/javascripts/ng-app/controllers/host/ipadPriceSetupController.js
+++ app/assets/javascripts/ng-app/controllers/host/ipadPriceSetupController.js
@@ -7,100 +7,7 @@ angular.module('BeConve')
             $location.path('/')
         });
 
-        $scope.ipadScreens = [
-            {
-                name: 'ipad 2',
-                price: 0
-            },
-            {
-                name: 'ipad 3',
-                price: 0
-            },
-            {
-                name: 'ipad 4',
-                price: 0
-            },
-            {
-                name: 'ipad mini',
-                price: 0
-            },
-            {
-                name: 'ipad mini 2',
-                price: 0
-            },
-            {
-                name: 'ipad mini 3',
-                price: 0
-            },
-            {
-                name: 'ipad air',
-                price: 0
-            }
-        ];
-
-        $scope.ipadBatteries = [
-            {
-                name: 'ipad 2',
-                price: 0
-            },
-            {
-                name: 'ipad 3',
-                price: 0
-            },
-            {
-                name: 'ipad 4',
-                price: 0
-            },
-            {
-                name: 'ipad mini',
-                price: 0
-            },
-            {
-                name: 'ipad mini 2',
-                price: 0
-            },
-            {
-                name: 'ipad mini 3',
-                price: 0
-            },
-            {
-                name: 'ipad air',
-                price: 0
-            }
-        ];
-
-        $scope.ipadNotOns = [
-            {
-                name: 'ipad 2',
-                price: 0
-            },
-            {
-                name: 'ipad 3',
-                price: 0
-            },
-            {
-                name: 'ipad 4',
-                price: 0
-            },
-            {
-                name: 'ipad mini',
-                price: 0
-            },
-            {
-                name: 'ipad mini 2',
-                price: 0
-            },
-            {
-                name: 'ipad mini 3',
-                price: 0
-            },
-            {
-                name: 'ipad air',
-                price: 0
-            }
-        ];
-
-        $scope.ipadLiquids = [
+        $scope.ipadModels = [
             {
                 name: 'ipad 2',
                 price: 0
@@ -132,23 +39,10 @@ angular.module('BeConve')
         ];
 
         //IPAD
-        $scope.ipadBrokenScreenSubmit = function(){
-            console.log('Inside ipad broken screen submit');
-            console.log($scope.ipadScreens);
-        };
-
-        $scope.ipadFaultyBatterySubmit = function(){
-            console.log('Inside ipad faulty battery submit');
-            console.log($scope.ipadBatteries);
-        };
-
-        $scope.ipadNotTurnOnSubmit = function(){
-            console.log('Inside ipad not on submit');
-            console.log($scope.ipadNotOns);
-        };
-        $scope.ipadLiquidDamageSubmit = function(){
-            console.log('Inside ipad liquid damage submit');
-            console.log($scope.ipadLiquids);
+        $scope.ipadPriceSubmit = function(model, type){
+            console.log('Inside ipad' + type + ' submit');
+            console.log(model.name);
+            console.log(model.price);
         };
 
     }]);
