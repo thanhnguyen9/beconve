7bee9feed87798458bf24e3ed6be22cece036a1c
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-09-17 00:16:51 -0500

    Set up prices for samsung

--- app/assets/javascripts/ng-app/controllers/host/samsungPriceSetupController.js
+++ app/assets/javascripts/ng-app/controllers/host/samsungPriceSetupController.js
@@ -1,44 +1,52 @@
 angular.module('BeConve')
-    .controller('samsungPriceSetupController', ['$scope', '$location', 'Auth', function($scope, $location, Auth) {
+    .controller('samsungPriceSetupController', ['$scope', '$location', 'Auth', '$http', 'Price', function($scope, $location, Auth, $http, Price) {
 
         Auth.currentUser().then(function(user) {
-            console.log(Auth._currentUser);
+            $scope.newIssue = function(issue) {
+                $scope.prices = {};
+                $scope.prices.type = 'samsung';
+                $scope.prices.issue = issue;
+                $scope.loading = true;
+
+                $http({
+                    method: 'GET',
+                    url: '/api/v1/prices/',
+                    params: $scope.prices
+                }).then(function successCallback(response) {
+                    if(response.data.response.status === 'success'){
+                        $scope.models = response.data.response.prices;
+                        $scope.loading = false;
+                    }else{
+                        $scope.error = 'Something went wrong. Please refresh the page';
+                        $scope.loading = false;
+                    }
+                }, function errorCallback(response) {
+                    $scope.error = 'Something went wrong. Please refresh the page';
+                    $scope.loading = false;
+                });
+            };
+
         }, function(error) {
             $location.path('/')
         });
 
-        $scope.samsungModels = [
-            {
-                name: 'note',
-                price: 0
-            },
-            {
-                name: 'note 2',
-                price: 0
-            },
-            {
-                name: 'note 3',
-                price: 0
-            },
-            {
-                name: 'S3',
-                price: 0
-            },
-            {
-                name: 'S4',
-                price: 0
-            },
-            {
-                name: 'S5',
-                price: 0
-            }
-        ];
-
-        //SAMSUNG
-        $scope.samsungPriceSubmit = function(model, type){
-            console.log('inside samsung' + type + 'submit');
-            console.log(model.name);
-            console.log(model.price);
+        //IPHONE
+        $scope.samsungPriceSubmit = function(model, issue){
+            $scope.updateLoading = true;
+            $scope.price = Price.get({ id: model.id }, function() {
+                // $scope.entry is fetched from server and is an instance of Entry
+                $scope.price.price = {};
+                $scope.price.price.price = parseInt(model.price);
+                $scope.price.$update({id: model.id}, function(res) {
+                    if(res.response === 'success'){
+                        $scope.info_message = 'Success';
+                        $scope.updateLoading = false;
+                    }else{
+                        $scope.error = 'Something went wrong. Please try again';
+                        $scope.updateLoading = false;
+                    }
+                });
+            });
         };
 
     }]);
